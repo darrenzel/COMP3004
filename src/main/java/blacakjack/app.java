469df51c9ef1e621c2b4290dee60a8f03b0deda9
darrenzel <darrenbian@cmail.carleton.ca>
2018-09-17 13:48:18 -0400

    implementing decks as arrayList

--- src/main/java/blacakjack/app.java
+++ src/main/java/blacakjack/app.java
@@ -1,6 +1,19 @@
 package blacakjack;
+import java.util.*;
 
 public class app {
+	
+	public static final String[] suite = {"H", "S", "C", "D"};
+	public static final String[] rank = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	
 	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Select console(c) input or file (F) input");
+		
+		
+		List<Cards> deck = new ArrayList<Cards>();
+		
 	}
+	 
 }
